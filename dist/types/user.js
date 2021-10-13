796e3803a9caeb6a25b48f999b5a262d09f35684
Damon Gillombardo <enigmaoneofficial@gmail.com>
2021-10-12 22:52:41 -0400

    Delete dist directory

--- dist/types/user.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IUser = void 0;
-const mongoose_1 = require("mongoose");
-exports.IUser = (0, mongoose_1.model)("User", new mongoose_1.Schema({
-    id: { type: String, required: true },
-    prefix: { type: String, default: "!" },
-}));
